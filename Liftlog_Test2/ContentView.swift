ee6af7a7b7a9e7a871d8c9650ce7b42d1f9edb08
Stephen Dawes <stephenrdawes@gmail.com>
2025-01-25 11:43:13 +0000

    Initial Commit

---
+++ Liftlog_Test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Liftlog_Test2
+//
+//  Created by Stephen Dawes on 25/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Steve!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Liftlog_Test2/Liftlog_Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Liftlog_Test2App.swift
+//  Liftlog_Test2
+//
+//  Created by Stephen Dawes on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Liftlog_Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
